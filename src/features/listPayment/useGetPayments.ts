9b173530f966977488118f98684301bbe03cf01d
Kosuke Numata <coursek8814@gmail.com>
2024-10-31 04:23:20 +0900

    feat: ユーザーごとの支払い情報を取得できるようにした

--- src/features/listPayment/useGetPayments.ts
+++ src/features/listPayment/useGetPayments.ts
@@ -1,17 +1,27 @@
-import { collection, getDocs } from "firebase/firestore"
+import { getAuth } from "firebase/auth"
+import { collection, doc, getDocs, query, where } from "firebase/firestore"
+import { useCallback } from "react"
 import { useFirestore } from "../../networks/firebase"
 import type { Payment } from "../../types/payment"
+import { userAuth } from "../../utils/auth/AuthProvider"
 
 interface UseGetPaymentsReturn {
   getPayments: () => Promise<Payment[]>
 }
 
 export function useGetPayments(): UseGetPaymentsReturn {
+  const auth = getAuth()
   const db = useFirestore()
 
-  async function getPayments(): Promise<Payment[]> {
+  const getPayments = useCallback(async () => {
+    // FIXME: 要リファクタリング - ページ読み込み時に支払い情報を取得できない
+    if (!auth.currentUser) return []
+
+    const userDocRef = doc(db, "users", auth.currentUser.uid)
     const paymentsCollection = collection(db, "payments")
-    const result = await getDocs(paymentsCollection)
+    const result = await getDocs(
+      query(paymentsCollection, where("user_id", "==", userDocRef)),
+    )
 
     return result.docs.map((doc) => {
       const data = doc.data()
@@ -25,7 +35,7 @@ export function useGetPayments(): UseGetPaymentsReturn {
         updatedDate: data.updatedDate as Date,
       }
     })
-  }
+  }, [db, auth])
 
   return {
     getPayments: getPayments,
