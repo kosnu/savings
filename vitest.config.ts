f23cd18e864e67963e4bfd51b578260aa56e85a4
Kosuke Numata <coursek8814@gmail.com>
2024-11-16 22:44:29 +0900

    feat: TestingLibraryを導入した

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,5 +3,7 @@ import { defineConfig } from "vitest/config"
 export default defineConfig({
   test: {
     globalSetup: ["./src/test/globalSetup.ts"],
+    environment: "jsdom",
+    setupFiles: ["./vitest-setup.ts"],
   },
 })
