6023befa9a0908127dd2dbfb4b1db6cac9e0972f
Kosuke Numata <coursek8814@gmail.com>
2025-04-28 16:21:31 +0900

    chore: MUIを使わないように変更した（トップページ）

---
+++ src/pages/TopPage/TopPage.stories.tsx
@@ -0,0 +1,26 @@
+import type { Meta, StoryObj } from "@storybook/react"
+import { FirestoreProvider } from "../../utils/firebase"
+import { TopPage } from "./TopPage"
+
+const meta = {
+  title: "Pages/TopPage",
+  component: TopPage,
+  parameters: {},
+  tags: ["autodocs"],
+  decorators: [
+    (Story) => (
+      <FirestoreProvider>
+        <Story />
+      </FirestoreProvider>
+    ),
+  ],
+  argTypes: {},
+  args: {},
+} satisfies Meta<typeof TopPage>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+  args: {},
+}
