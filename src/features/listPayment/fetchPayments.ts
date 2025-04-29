68d3755c9e9ea5f08ae099caf126b126d98db4f1
Kosuke Numata <coursek8814@gmail.com>
2025-04-29 17:34:20 +0900

    chore: 支払い情報取得処理を別関数に切り出した
    why:
    * 可読性を高めるため
    * 後のテストを書きやすくするため

---
+++ src/features/listPayment/fetchPayments.ts
@@ -0,0 +1,39 @@
+import type { User } from "firebase/auth"
+import {
+  type Firestore,
+  collection,
+  getDocs,
+  query,
+  where,
+} from "firebase/firestore"
+import type { Payment } from "../../types/payment"
+
+export async function fetchPayments(
+  db: Firestore,
+  user: User | null,
+): Promise<Payment[]> {
+  if (!user) {
+    return []
+  }
+
+  const paymentsRef = collection(db, `users/${user.uid}/payments`)
+  const querySnapshot = await getDocs(
+    query(paymentsRef, where("user_id", "==", user.uid)),
+  )
+
+  const payments = querySnapshot.docs.map((doc) => {
+    const data = doc.data()
+
+    // FIXME: as をやめてconverterを使う
+    return {
+      id: doc.id,
+      title: data.title as string,
+      price: data.price as number,
+      date: data.date as Date,
+      createdDate: data.createdDate as Date,
+      updatedDate: data.updatedDate as Date,
+    }
+  })
+
+  return payments
+}
