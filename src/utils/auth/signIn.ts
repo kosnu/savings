3b63e5ef3b243ddc8d0e08111bf49a540e0bf958
Kosuke Numata <coursek8814@gmail.com>
2025-04-29 17:17:04 +0900

    chore: サインイン方法をポップアップに変更した
    why: Safariなど他のブラウザでの動作が担保できないため

--- src/utils/auth/signIn.ts
+++ src/utils/auth/signIn.ts
@@ -1,8 +1,9 @@
 import {
   GoogleAuthProvider,
+  browserLocalPersistence,
   getAuth,
-  getRedirectResult,
-  signInWithRedirect,
+  setPersistence,
+  signInWithPopup,
 } from "firebase/auth"
 import type { Firestore } from "firebase/firestore"
 import { createUser } from "./createUser"
@@ -10,22 +11,19 @@ import { createUser } from "./createUser"
 export async function signIn(db: Firestore) {
   const provider = new GoogleAuthProvider()
   const auth = getAuth()
-  auth.onAuthStateChanged(async (user) => {
-    if (user) {
-    } else {
-      await signInWithRedirect(auth, provider)
-      const result = await getRedirectResult(auth)
-      const user = result?.user
-      if (!user) return
 
-      await createUser({
-        db: db,
-        value: {
-          id: user.uid,
-          email: user.email ?? "",
-          name: user.displayName ?? "NO NAME",
-        },
-      })
-    }
+  // セッション永続性を設定
+  await setPersistence(auth, browserLocalPersistence)
+
+  const result = await signInWithPopup(auth, provider)
+  if (!result?.user) return
+
+  await createUser({
+    db: db,
+    value: {
+      id: result.user.uid,
+      email: result.user.email ?? "",
+      name: result.user.displayName ?? "NO NAME",
+    },
   })
 }
