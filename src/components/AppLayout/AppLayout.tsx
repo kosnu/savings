d155c23a21e4990f4012fad1f01cdd0fed9d2018
Kosuke Numata <coursek8814@gmail.com>
2025-05-02 02:23:46 +0900

    chore: レイアウト（サイドバー）をRadixUIへ移行した

--- src/components/AppLayout/AppLayout.tsx
+++ src/components/AppLayout/AppLayout.tsx
@@ -1,205 +1,47 @@
-import {
-  ChevronLeft as ChevronLeftIcon,
-  ChevronRight as ChevronRightIcon,
-  Menu as MenuIcon,
-  Payments as PaymentsIcon,
-} from "@mui/icons-material"
-import {
-  Box,
-  CssBaseline,
-  Divider,
-  IconButton,
-  List,
-  ListItem,
-  ListItemButton,
-  ListItemIcon,
-  ListItemText,
-  Drawer as MuiDrawer,
-  Toolbar,
-  Typography,
-} from "@mui/material"
-import MuiAppBar, {
-  type AppBarProps as MuiAppBarProps,
-} from "@mui/material/AppBar"
-import {
-  type CSSObject,
-  type Theme,
-  styled,
-  useTheme,
-} from "@mui/material/styles"
-import { useCallback, useState } from "react"
-import { Outlet, Link as rrdLink } from "react-router-dom"
-
-const drawerWidth = 240
+import { CardStackIcon } from "@radix-ui/react-icons"
+import { Button, Flex, Separator, Text } from "@radix-ui/themes"
+import { useState } from "react"
+import { Link, Outlet } from "react-router-dom"
+import styles from "./AppLayout.module.css"
 
 export function AppLayout() {
-  const theme = useTheme()
-  const [open, setOpen] = useState(false)
-
-  const handleDrawerOpen = useCallback(() => {
-    setOpen(true)
-  }, [])
-
-  const handleDrawerClose = useCallback(() => {
-    setOpen(false)
-  }, [])
+  const [isSidebarOpen] = useState(true)
 
   return (
-    <Box sx={{ display: "flex" }}>
-      <CssBaseline />
-      <AppBar position="fixed" open={open}>
-        <Toolbar>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            onClick={handleDrawerOpen}
-            edge="start"
-            sx={[
-              {
-                marginRight: 5,
-              },
-              open && { display: "none" },
-            ]}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" noWrap component="div">
-            <Link to="/">Savings</Link>
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      <Drawer variant="permanent" open={open}>
-        <DrawerHeader>
-          <IconButton onClick={handleDrawerClose}>
-            {theme.direction === "rtl" ? (
-              <ChevronRightIcon />
-            ) : (
-              <ChevronLeftIcon />
-            )}
-          </IconButton>
-        </DrawerHeader>
-        <Divider />
-        <List>
-          <ListItem disablePadding sx={{ display: "block" }}>
-            <Link to="/payments">
-              <ListItemButton
-                sx={{
-                  minHeight: 48,
-                  px: 2.5,
-                  justifyContent: open ? "initial" : "center",
-                }}
-              >
-                <ListItemIcon
-                  sx={{
-                    minWidth: 0,
-                    justifyContent: "center",
-                    mr: open ? 3 : "auto",
-                  }}
-                >
-                  <PaymentsIcon />
-                </ListItemIcon>
-                <ListItemText
-                  primary="Payments"
-                  sx={{
-                    opacity: open ? 1 : 0,
-                  }}
-                />
-              </ListItemButton>
-            </Link>
-          </ListItem>
-        </List>
-      </Drawer>
-      <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
-        <DrawerHeader />
+    <Flex className={styles.layout}>
+      {/* Sidebar */}
+      <aside
+        className={`${styles.sidebar} ${isSidebarOpen ? styles.open : styles.closed}`}
+      >
+        {/* Sidebar Header */}
+        <Flex className={styles.sidebarHeader} p="4">
+          <Link to="/" className={styles.link}>
+            <Button className={styles.sidebarButton} variant="ghost" size="3">
+              <Text size="3" weight="bold">
+                My Savings
+              </Text>
+            </Button>
+          </Link>
+        </Flex>
+        <Separator size="4" />
+        {/* Sidebar Contents */}
+        <Flex className={styles.sidebarContents} align="start" gap="2" p="4">
+          <Link to="/payments" className={styles.link}>
+            <Button className={styles.sidebarButton} variant="ghost" size="3">
+              <CardStackIcon />
+              Payments
+            </Button>
+          </Link>
+        </Flex>
+        <Separator size="4" />
+        {/* Sidebar Footer */}
+        <Flex className={styles.sidebarFooter} p="4" />
+      </aside>
+
+      {/* Main Content */}
+      <main className={styles.main}>
         <Outlet />
-      </Box>
-    </Box>
+      </main>
+    </Flex>
   )
 }
-
-const openedMixin = (theme: Theme): CSSObject => ({
-  width: drawerWidth,
-  transition: theme.transitions.create("width", {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.enteringScreen,
-  }),
-  overflowX: "hidden",
-})
-
-const closedMixin = (theme: Theme): CSSObject => ({
-  transition: theme.transitions.create("width", {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen,
-  }),
-  overflowX: "hidden",
-  width: `calc(${theme.spacing(7)} + 1px)`,
-  [theme.breakpoints.up("sm")]: {
-    width: `calc(${theme.spacing(8)} + 1px)`,
-  },
-})
-
-const DrawerHeader = styled("div")(({ theme }) => ({
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "flex-end",
-  padding: theme.spacing(0, 1),
-  // necessary for content to be below app bar
-  ...theme.mixins.toolbar,
-}))
-
-interface AppBarProps extends MuiAppBarProps {
-  open?: boolean
-}
-
-const AppBar = styled(MuiAppBar, {
-  shouldForwardProp: (prop) => prop !== "open",
-})<AppBarProps>(({ theme }) => ({
-  zIndex: theme.zIndex.drawer + 1,
-  transition: theme.transitions.create(["width", "margin"], {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen,
-  }),
-  variants: [
-    {
-      props: ({ open }) => open,
-      style: {
-        marginLeft: drawerWidth,
-        width: `calc(100% - ${drawerWidth}px)`,
-        transition: theme.transitions.create(["width", "margin"], {
-          easing: theme.transitions.easing.sharp,
-          duration: theme.transitions.duration.enteringScreen,
-        }),
-      },
-    },
-  ],
-}))
-
-const Drawer = styled(MuiDrawer, {
-  shouldForwardProp: (prop) => prop !== "open",
-})(({ theme }) => ({
-  width: drawerWidth,
-  flexShrink: 0,
-  whiteSpace: "nowrap",
-  boxSizing: "border-box",
-  variants: [
-    {
-      props: ({ open }) => open,
-      style: {
-        ...openedMixin(theme),
-        "& .MuiDrawer-paper": openedMixin(theme),
-      },
-    },
-    {
-      props: ({ open }) => !open,
-      style: {
-        ...closedMixin(theme),
-        "& .MuiDrawer-paper": closedMixin(theme),
-      },
-    },
-  ],
-}))
-
-const Link = styled(rrdLink)(() => ({
-  textDecoration: "none",
-  color: "inherit",
-}))
