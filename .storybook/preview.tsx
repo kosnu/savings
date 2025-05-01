d00df5668becc8ead114c0815fb6c9ea624c2a89
Kosuke Numata <coursek8814@gmail.com>
2025-05-01 22:27:34 +0900

    chore: RadixUIをStorybook上で描画するための準備

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,7 +1,11 @@
 import type { Preview } from "@storybook/react"
 import React from "react"
-import "../src/assets/global.module.css"
 import { BrowserRouter } from "react-router-dom"
+import { ThemeProvider } from "../src/utils/theme/ThemeProvider"
+
+import "@radix-ui/themes/styles.css"
+import "../src/assets/global.module.css"
+import "../src/assets/radixTheme.module.css"
 
 const preview: Preview = {
   parameters: {
@@ -14,9 +18,11 @@ const preview: Preview = {
   },
   decorators: [
     (Story) => (
-      <BrowserRouter>
-        <Story />
-      </BrowserRouter>
+      <ThemeProvider>
+        <BrowserRouter>
+          <Story />
+        </BrowserRouter>
+      </ThemeProvider>
     ),
   ],
 }
