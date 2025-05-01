2cad21a1fc50c2c75eb82360eb2ff004ead96a13
Kosuke Numata <coursek8814@gmail.com>
2025-05-01 17:55:21 +0900

    chore: 自前のテキストフィールドをリファクタリングした

--- src/components/inputs/Textfield/Textfield.tsx
+++ src/components/inputs/Textfield/Textfield.tsx
@@ -1,28 +1,22 @@
-import { type ComponentProps, useId } from "react"
-import styles from "./Textfield.module.css"
+import { Text, TextField } from "@radix-ui/themes"
+import { useId } from "react"
 
-type InputProps = Omit<ComponentProps<"input">, "name">
-
-interface TextfieldProps extends InputProps {
+interface TextfieldProps {
   label: React.ReactNode
   name: string
+  placeholder?: string
+  type?: "text" | "email" | "password" | "number"
+  required?: boolean
 }
 
 export function Textfield({ label, name, ...props }: TextfieldProps) {
   const id = useId()
   return (
-    <div className={styles.textfieldRoot}>
-      <fieldset className={styles.textfieldFieldset}>
-        <legend>
-          <label htmlFor={name}>{label}</label>
-        </legend>
-        <input
-          {...props}
-          id={`${name}-${id}`}
-          name={name}
-          className={styles.textfieldInput}
-        />
-      </fieldset>
-    </div>
+    <label>
+      <Text as="div" size="2" mb="1" weight="bold">
+        {label}
+      </Text>
+      <TextField.Root {...props} id={`${name}-${id}`} name={name} />
+    </label>
   )
 }
