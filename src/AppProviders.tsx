7a57bdebca8afb2799bfc17742b2506bf35254d0
Kosuke Numata <coursek8814@gmail.com>
2025-05-03 16:33:38 +0900

    remove: MUIをアンインストールした

--- src/AppProviders.tsx
+++ src/AppProviders.tsx
@@ -1,5 +1,3 @@
-import { LocalizationProvider } from "@mui/x-date-pickers"
-import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFnsV3"
 import type { ReactNode } from "react"
 import { FirestoreProvider } from "./utils/firebase"
 import { ThemeProvider } from "./utils/theme/ThemeProvider"
@@ -12,9 +10,7 @@ export function AppProviders({ children }: AppProvidersProps) {
   return (
     <>
       <FirestoreProvider>
-        <LocalizationProvider dateAdapter={AdapterDateFns}>
-          <ThemeProvider>{children}</ThemeProvider>
-        </LocalizationProvider>
+        <ThemeProvider>{children}</ThemeProvider>
       </FirestoreProvider>
     </>
   )
