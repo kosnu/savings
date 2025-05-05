50a0258366135b857ecfde571cab765e7e295a41
Kosuke Numata <coursek8814@gmail.com>
2025-05-05 21:10:47 +0900

    remove: 旧コンポーネントを削除した

--- src/components/buttons/OutlinedButton/OutlinedButton.tsx
+++
@@ -1,8 +0,0 @@
-import { ButtonBase, type ButtonBaseProps } from "../ButtonBase"
-import styles from "./OutlinedButton.module.css"
-
-export interface OutlinedButtonProps extends ButtonBaseProps {}
-
-export function OutlinedButton({ ...props }: OutlinedButtonProps) {
-  return <ButtonBase {...props} className={styles.buttonPrimary} />
-}
