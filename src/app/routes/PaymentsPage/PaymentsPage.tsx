19693eb8e51673093a9ea7f0ffb82fec3408bb6e
Kosuke Numata <coursek8814@gmail.com>
2025-05-06 12:03:52 +0900

    chore: 支払い一覧ページの最大サイズを指定した

--- src/app/routes/PaymentsPage/PaymentsPage.tsx
+++ src/app/routes/PaymentsPage/PaymentsPage.tsx
@@ -1,4 +1,4 @@
-import { Flex } from "@radix-ui/themes"
+import { Container, Flex } from "@radix-ui/themes"
 import { useCallback, useState } from "react"
 import { CreatePaymentModal } from "../../../features/createPayment"
 import { PaymentList } from "../../../features/listPayment"
@@ -14,12 +14,14 @@ export function PaymentsPage() {
 
   return (
     <>
-      <Flex direction="column" gap="3">
-        <Flex justify="end" align="center" gap="3">
-          <CreatePaymentModal onSuccess={handleCreateSuccess} />
+      <Container size="4">
+        <Flex direction="column" gap="3">
+          <Flex justify="end" align="center" gap="3">
+            <CreatePaymentModal onSuccess={handleCreateSuccess} />
+          </Flex>
+          <PaymentList key={refreshKey} />
         </Flex>
-        <PaymentList key={refreshKey} />
-      </Flex>
+      </Container>
     </>
   )
 }
