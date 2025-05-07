6459e848435bb4cad709be647f21be09cc73a61d
Kosuke Numata <coursek8814@gmail.com>
2025-05-07 21:58:40 +0900

    chore: 支払いページへのURL生成処理に年月を指定できるように変更した

--- src/config/paths.ts
+++ src/config/paths.ts
@@ -5,7 +5,13 @@ export const paths = {
   },
   payments: {
     path: "/payments",
-    getHref: () => "/payments",
+    getHref: (year?: string, month?: string) => {
+      const params = new URLSearchParams()
+      if (year) params.set("year", year)
+      if (month) params.set("month", month)
+
+      return `/payments${params.toString() ? `?${params.toString()}` : ""}`
+    },
   },
   // Add more paths as needed
 } as const
