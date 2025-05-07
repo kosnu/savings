9749fe318663f3d26b0999d026b853371f33faa0
Kosuke Numata <coursek8814@gmail.com>
2025-05-07 22:02:27 +0900

    chore: サイドバー用ツリーボタンを利用するように変更した

--- src/app/AppLayout/AppLayout.tsx
+++ src/app/AppLayout/AppLayout.tsx
@@ -1,7 +1,8 @@
-import { CardStackIcon } from "@radix-ui/react-icons"
+import { CalendarIcon } from "@radix-ui/react-icons"
 import { Button, Flex, Separator, Text } from "@radix-ui/themes"
 import { useState } from "react"
 import { Link, Outlet } from "react-router-dom"
+import { SidebarTreeButton } from "../../components/buttons/SidebarTreeButton"
 import { paths } from "../../config/paths"
 import styles from "./AppLayout.module.css"
 
@@ -27,12 +28,7 @@ export function AppLayout() {
         <Separator size="4" />
         {/* Sidebar Contents */}
         <Flex className={styles.sidebarContents} align="start" gap="2" p="4">
-          <Link to={paths.payments.getHref()} className={styles.link}>
-            <Button className={styles.sidebarButton} variant="ghost" size="3">
-              <CardStackIcon />
-              Payments
-            </Button>
-          </Link>
+          <SidebarTreeButton treeObject={objects} />
         </Flex>
         <Separator size="4" />
         {/* Sidebar Footer */}
@@ -45,4 +41,54 @@ export function AppLayout() {
       </main>
     </Flex>
   )
+}
+
+// 仮のサンプルデータ
+const objects = {
+  id: "title",
+  icon: <CalendarIcon />,
+  label: "Payments",
+  children: [
+    {
+      id: "2025",
+      label: "2025年",
+      children: [
+        {
+          id: "01",
+          label: "1月",
+          href: paths.payments.getHref("2025", "01"),
+        },
+        {
+          id: "02",
+          label: "2月",
+          href: paths.payments.getHref("2025", "02"),
+        },
+        {
+          id: "03",
+          label: "3月",
+        },
+      ],
+    },
+    {
+      id: "2024",
+      label: "2024年",
+      children: [
+        {
+          id: "01",
+          label: "1月",
+          href: paths.payments.getHref("2024", "01"),
+        },
+        {
+          id: "02",
+          label: "2月",
+          href: paths.payments.getHref("2024", "02"),
+        },
+      ],
+    },
+    {
+      id: "2023",
+      label: "2023年",
+      children: [],
+    },
+  ],
 }
