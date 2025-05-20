2ca7518d25607356ca0d62c13fd6d9c5d4900498
Kosuke Numata <coursek8814@gmail.com>
2025-05-20 21:50:44 +0900

    chore: キャンセルボタンにonClickを追加した

--- src/components/buttons/CancelButton/CancelButton.tsx
+++ src/components/buttons/CancelButton/CancelButton.tsx
@@ -1,13 +1,17 @@
 import { Button } from "@radix-ui/themes"
-import type { ReactNode } from "react"
+import type { MouseEventHandler, ReactNode } from "react"
 
 interface CancelButtonProps {
   children?: ReactNode
+  onClick?: MouseEventHandler<HTMLButtonElement> | undefined
 }
 
-export function CancelButton({ children = "Cancel" }: CancelButtonProps) {
+export function CancelButton({
+  children = "Cancel",
+  onClick,
+}: CancelButtonProps) {
   return (
-    <Button variant="soft" color="gray">
+    <Button variant="soft" color="gray" onClick={onClick}>
       {children}
     </Button>
   )
