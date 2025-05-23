041ad4814b6616afb519073b74889944e41ed4d4
Kosuke Numata <coursek8814@gmail.com>
2025-05-23 18:32:16 +0900

    feat: 支払い情報の一項目をコンポーネント化した

---
+++ src/features/listPayment/PaymentItem/PaymentItem.tsx
@@ -0,0 +1,19 @@
+import { Table } from "@radix-ui/themes"
+import type { Payment } from "../../../types/payment"
+import { formatDateToLocaleString } from "../../../utils/formatter/formatDateToLocaleString"
+
+interface PaymentItemProps {
+  payment: Payment
+}
+
+export function PaymentItem({ payment }: PaymentItemProps) {
+  return (
+    <Table.Row>
+      <Table.RowHeaderCell>
+        {formatDateToLocaleString(payment.date)}
+      </Table.RowHeaderCell>
+      <Table.Cell>{payment.title}</Table.Cell>
+      <Table.Cell align="right">{payment.price}</Table.Cell>
+    </Table.Row>
+  )
+}
