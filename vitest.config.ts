2f3755e75b8fd88d023109b93a5c6906f4695377
Kosuke Numata <coursek8814@gmail.com>
2025-07-05 19:03:57 +0900

    chore: 色々な設定ファイルを整理した
    - Biome の対象ファイルの追加
    - vitest のセットアップファイルの命名を他と合わせた
    - package-lock の更新
    - Storybook + Vitest の Deprecated 対応

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,15 @@
+import path from "node:path"
+import { fileURLToPath } from "node:url"
+import { storybookTest } from "@storybook/addon-vitest/vitest-plugin"
 import { defineConfig } from "vitest/config"
+
+const dirname = path.dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
   test: {
     globalSetup: ["./src/test/globalSetup.ts"],
     environment: "jsdom",
-    setupFiles: ["./vitest-setup.ts"],
+    setupFiles: ["./vitest.setup.ts"],
     env: {
       // TODO: .env.test から読み取る
       VITE_FIREBASE_API_KEY: "xxx",
@@ -23,5 +28,32 @@ export default defineConfig({
         "Date",
       ],
     },
+    projects: [
+      {
+        extends: true,
+        plugins: [
+          // The plugin will run tests for the stories defined in your Storybook config
+          // See options at: https://storybook.js.org/docs/writing-tests/test-addon#storybooktest
+          storybookTest({
+            configDir: path.join(dirname, ".storybook"),
+            tags: {
+              exclude: ["skip"],
+            },
+            storybookScript: "npm run storybook --ci",
+            storybookUrl: process.env.SB_URL,
+          }),
+        ],
+        test: {
+          name: "storybook",
+          browser: {
+            enabled: true,
+            headless: true,
+            provider: "playwright",
+            instances: [{ browser: "chromium" }],
+          },
+          setupFiles: [".storybook/vitest.setup.ts"],
+        },
+      },
+    ],
   },
 })
