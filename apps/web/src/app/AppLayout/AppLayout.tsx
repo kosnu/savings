43469eaa81e60fba5dd6481ba318d18a7c646a92
Kosuke Numata <coursek8814@gmail.com>
2025-08-05 00:43:01 +0900

    feat: サイドバーを開閉できるようにした
    Why: スマホ対応するには開閉できるようにならないと表示領域が縮んでしまうため

--- apps/web/src/app/AppLayout/AppLayout.tsx
+++ apps/web/src/app/AppLayout/AppLayout.tsx
@@ -1,5 +1,6 @@
-import { Button, Flex, Separator, Text } from "@radix-ui/themes"
-import { useState } from "react"
+import { HamburgerMenuIcon } from "@radix-ui/react-icons"
+import { Button, Flex, IconButton, Separator, Text } from "@radix-ui/themes"
+import { useCallback, useState } from "react"
 import { Link, Outlet } from "react-router-dom"
 import { SidebarTreeButton } from "../../components/buttons/SidebarTreeButton"
 import { paths } from "../../config/paths"
@@ -7,7 +8,11 @@ import styles from "./AppLayout.module.css"
 import { getPaymentsSidebarTree } from "./getPaymentsSidebarTree"
 
 export function AppLayout() {
-  const [isSidebarOpen] = useState(true)
+  const [isSidebarOpen, setIsSideberOpen] = useState(true)
+
+  const handleMenuClick = useCallback(() => {
+    setIsSideberOpen((value) => !value)
+  }, [])
 
   return (
     <Flex className={styles.layout}>
@@ -35,10 +40,17 @@ export function AppLayout() {
         <Flex className={styles.sidebarFooter} p="4" />
       </aside>
 
-      {/* Main Content */}
-      <main className={styles.main}>
-        <Outlet />
-      </main>
+      <Flex direction="column" flexGrow="1">
+        <header className={styles.header}>
+          <IconButton variant="surface" onClick={handleMenuClick}>
+            <HamburgerMenuIcon width="18" height="18" />
+          </IconButton>
+        </header>
+        {/* Main Content */}
+        <main className={styles.main}>
+          <Outlet />
+        </main>
+      </Flex>
     </Flex>
   )
 }
