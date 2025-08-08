923294c3d6c8e3f23490d40be09fa68a3e95af72
Kosuke Numata <coursek8814@gmail.com>
2025-08-09 03:04:09 +0900

    chore: サイドバーやヘッダーをコンポーネントに置き換えた
    Why: リファクタリングしやすくするため

--- apps/web/src/app/AppLayout/AppLayout.tsx
+++ apps/web/src/app/AppLayout/AppLayout.tsx
@@ -1,9 +1,9 @@
 import { HamburgerMenuIcon } from "@radix-ui/react-icons"
-import { Button, Flex, IconButton, Separator, Text } from "@radix-ui/themes"
+import { Flex, IconButton } from "@radix-ui/themes"
 import { useCallback, useState } from "react"
-import { Link, Outlet } from "react-router-dom"
+import { Outlet } from "react-router-dom"
 import { SidebarTreeButton } from "../../components/buttons/SidebarTreeButton"
-import { paths } from "../../config/paths"
+import { Sidebar } from "../../shared/sidebar"
 import styles from "./AppLayout.module.css"
 import { getPaymentsSidebarTree } from "./getPaymentsSidebarTree"
 
@@ -17,40 +17,33 @@ export function AppLayout() {
   return (
     <Flex className={styles.layout}>
       {/* Sidebar */}
-      <aside
-        className={`${styles.sidebar} ${isSidebarOpen ? styles.open : styles.closed}`}
-      >
-        {/* Sidebar Header */}
-        <Flex className={styles.sidebarHeader} p="4">
-          <Link to={paths.root.getHref()} className={styles.link}>
-            <Button className={styles.sidebarButton} variant="ghost" size="3">
-              <Text size="3" weight="bold">
-                My Savings
-              </Text>
-            </Button>
-          </Link>
-        </Flex>
-        <Separator size="4" />
-        {/* Sidebar Contents */}
+      <Sidebar open={isSidebarOpen}>
         <Flex className={styles.sidebarContents} align="start" gap="2" p="4">
           <SidebarTreeButton treeObject={getPaymentsSidebarTree(new Date())} />
         </Flex>
-        <Separator size="4" />
-        {/* Sidebar Footer */}
-        <Flex className={styles.sidebarFooter} p="4" />
-      </aside>
+      </Sidebar>
 
       <Flex direction="column" flexGrow="1">
-        <header className={styles.header}>
-          <IconButton variant="surface" onClick={handleMenuClick}>
-            <HamburgerMenuIcon width="18" height="18" />
-          </IconButton>
-        </header>
+        <Header onMenuClick={handleMenuClick} />
         {/* Main Content */}
         <main className={styles.main}>
           <Outlet />
         </main>
       </Flex>
     </Flex>
+  )
+}
+
+interface HeaderProps {
+  onMenuClick: () => void
+}
+
+function Header({ onMenuClick }: HeaderProps) {
+  return (
+    <header className={styles.header}>
+      <IconButton variant="surface" onClick={onMenuClick}>
+        <HamburgerMenuIcon width="18" height="18" />
+      </IconButton>
+    </header>
   )
 }
