653187aa0f1da00dcb9e578a5b797a9afe75563d
Kosuke Numata <coursek8814@gmail.com>
2025-08-10 18:17:33 +0900

    chore: サイドバーをmain要素から浮いた状態で表示されるようにした
    Why: 左側に表示させる場合、スマホのときにUIが崩れてしまうため
    また、その崩れを防止する策を考えないといけないため

--- apps/web/src/shared/sidebar/components/Sidebar/Sidebar.tsx
+++ apps/web/src/shared/sidebar/components/Sidebar/Sidebar.tsx
@@ -1,19 +1,27 @@
-import { Button, Flex, Separator, Text } from "@radix-ui/themes"
+import { Cross1Icon } from "@radix-ui/react-icons"
+import { Button, Flex, IconButton, Separator, Text } from "@radix-ui/themes"
 import type { ReactNode } from "react"
 import { Link } from "react-router-dom"
 import { paths } from "../../../../config/paths"
 import styles from "./Sidebar.module.css"
 
 interface SidebarProps {
-  open: boolean
   children?: ReactNode
+  open: boolean
+  onClose: () => void
 }
 
-export function Sidebar({ open, children }: SidebarProps) {
+export function Sidebar({ children, open, onClose }: SidebarProps) {
   return (
     <aside data-open={open} className={styles.sidebar}>
       {/* Sidebar Header */}
-      <Flex className={styles.sidebarHeader} p="4">
+      <Flex
+        className={styles.sidebarHeader}
+        p="4"
+        justify="between"
+        align="center"
+        gap="4"
+      >
         <Link to={paths.root.getHref()} className={styles.link}>
           <Button className={styles.sidebarButton} variant="ghost" size="3">
             <Text size="3" weight="bold">
@@ -21,10 +29,15 @@ export function Sidebar({ open, children }: SidebarProps) {
             </Text>
           </Button>
         </Link>
+        <IconButton variant="ghost" onClick={onClose}>
+          <Cross1Icon />
+        </IconButton>
       </Flex>
       <Separator size="4" />
       {/* Sidebar Contents */}
-      {children}
+      <Flex className={styles.sidebarContents} align="start" gap="2" p="4">
+        {children}
+      </Flex>
       <Separator size="4" />
       {/* Sidebar Footer */}
       <Flex className={styles.sidebarFooter} p="4" />
