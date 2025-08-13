ab06ac9e5978a7f926767abb55dfd63dfc7352d0
Kosuke Numata <coursek8814@gmail.com>
2025-08-14 00:15:35 +0900

    build: 配信時にキャッシュを最大限利用できるようなビルド設定に変更した

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -9,4 +9,28 @@ export default defineConfig({
       localsConvention: "dashes",
     },
   },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            if (
+              id.includes("react-dom") ||
+              id.includes("react-router-dom") ||
+              id.includes("react")
+            ) {
+              return "vendor-react"
+            }
+            if (id.includes("firebase")) {
+              return "vendor-firebase"
+            }
+            if (id.includes("@radix-ui")) {
+              return "vendor-radix"
+            }
+            return "vendor"
+          }
+        },
+      },
+    },
+  },
 })
