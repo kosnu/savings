b1ee13e9cd0ea3d440ff43489053b7b31aff372f
Kosuke Numata <coursek8814@gmail.com>
2025-09-13 19:32:10 +0900

    chore: カテゴリ情報が見つからなかった時のデータを統一した

---
+++ apps/web/src/features/categories/unknownCategory.ts
@@ -0,0 +1,8 @@
+import type { Category } from "../../types/category"
+
+export const unknownCategory: Category = {
+  id: "unknown",
+  name: "Unknown category",
+  createdDate: new Date(),
+  updatedDate: new Date(),
+}
