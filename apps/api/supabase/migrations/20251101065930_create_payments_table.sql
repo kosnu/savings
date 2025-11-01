7988e8e009f5a2fcd3994f9c23b6eab8787ed53c
Kosuke Numata <coursek8814@gmail.com>
2025-11-02 02:42:25 +0900

    feat: 初期テーブルのマイグレーションファイルを作成した

---
+++ apps/api/supabase/migrations/20251101065930_create_payments_table.sql
@@ -0,0 +1,13 @@
+create table payments (
+    id bigint generated always as identity primary key,
+    note varchar(255),
+    amount numeric(10, 2) not null,
+    date date not null,
+    created_at timestamp with time zone default current_timestamp,
+    updated_at timestamp with time zone default current_timestamp,
+
+    category_id bigint references categories(id) on delete set null,
+    user_id bigint references users(id) on delete set null
+);
+
+create index on payments (user_id, date, category_id, created_at);
