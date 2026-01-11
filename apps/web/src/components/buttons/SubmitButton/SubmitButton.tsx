9b8285cd4a3d80c8a4743a309f02015a18fcee05
Kosuke Numata <coursek8814@gmail.com>
2026-01-11 15:13:38 +0900

    chore: ボタンの非活性化のロジックを修正した
    Why: プロパティの上書きを防ぐため

--- apps/web/src/components/buttons/SubmitButton/SubmitButton.tsx
+++ apps/web/src/components/buttons/SubmitButton/SubmitButton.tsx
@@ -5,10 +5,11 @@ type SubmitButtonProps = ButtonProps
 export function SubmitButton({
   children,
   loading,
+  disabled,
   ...props
 }: SubmitButtonProps) {
   return (
-    <Button type="submit" disabled={loading} {...props}>
+    <Button type="submit" {...props} disabled={loading || disabled}>
       {loading && <Spinner aria-label="loading-spinner" />}
       {children}
     </Button>
